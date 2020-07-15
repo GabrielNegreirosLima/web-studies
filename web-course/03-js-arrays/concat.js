702602f672f921035221ddb020d6e07dd6840310
Gabriel Negreiros Lima <gabrielnegreiroslima@gmail.com>
2020-07-15 18:28:30 -0300

    Add Concat lesson

---
+++ web-course/03-js-arrays/concat.js
@@ -0,0 +1,7 @@
+const women = ['Ana', 'Alice']
+const men = ['Bob', 'Carl']
+const all = women.concat(men, 'Other person')
+
+console.log(all, men, women)
+
+console.log(['a', 'b'].concat([1, 2], [3, 4], 5, [[6, 7]]))
