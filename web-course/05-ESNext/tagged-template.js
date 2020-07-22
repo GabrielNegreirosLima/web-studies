cde5686ae9cb2dda8e70244706818743a8d111bd
Gabriel Negreiros Lima <gabrielnegreiroslima@gmail.com>
2020-07-22 19:53:32 -0300

    Add more template string manipulations

--- web-course/05-ESNext/tagged-template.js
+++ web-course/05-ESNext/tagged-template.js
@@ -8,3 +8,16 @@ const student = 'Gabriel'
 const state = 'Approved'
 console.log(tag `${student} is ${state}!`)
 
+
+function toReal(pieces, ...values){
+	const result = []
+	values.forEach((value, index) => {
+		value = isNaN(value) ? value : `R$${value.toFixed(2)}`
+		result.push(pieces[index], value)
+	})
+	return result.join('')
+}
+
+const price = 29.99
+const piece = 10
+console.log(toReal `1x is ${price} or 3x of ${piece}`)
